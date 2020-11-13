8226523739c8c1f3ee506c4df10ab63ec7a96ecf
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-11-13 23:08:19 +0530

    finished the prediction.py

--- prediction.py
+++ prediction.py
@@ -6,18 +6,20 @@ import os
 import sys
 
 
-def loadimage(file):
-    with open(file, "rb") as f:
-        img = Image.open(f.read())
-    return img
+def loadimage(filename):
+    train = []
+    with open(os.path.join("validation/", filename), "rb") as img:
+        trainimage = Image.open(img).convert("L")
+        trainimage = np.asarray(trainimage)
+        train.append(trainimage)
+    return np.asarray(train)
 
 
 if __name__ == "__main__":
-    og = loadimage("human01-14.png")
-    test = np.asarray(og.convert("L"))
+    og = loadimage("valhuman03-05.png")
     class_names = ["horse", "human"]
     model = keras.models.load_model("hvm_f1.h5")
-    prediction = model.evaluate(test)
-    plt.imshow(og, cmp="RGB")
+    prediction = model.evaluate(og)
+    plt.imshow(og[0], cmap="gray")
     plt.title(class_names[np.argmax(prediction)])
     plt.show()
