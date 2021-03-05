8947d7d0169250d13edce4ff423a08b2f0a93a92
jvs-alpha <jvsamuel.rufus@gmail.com>
2021-03-05 17:57:37 +0530

    added changes to hvmtrain2.py

--- hvmtrain2.py
+++ hvmtrain2.py
@@ -13,20 +13,27 @@ if __name__ == "__main__":
         image_size=(300,300),
         batch_size=345
     )
-    class_names = train.class_names
-    print(train.images)
-    # model = keras.Sequential([
-    #     keras.layers.Flatten(input_shape=(300,300)),
-    #     keras.layers.Dense(130,activation="relu"),
-    #     # keras.layers.Dense(80,activation="relu"),
-    #     keras.layers.Dense(2,activation="softmax")
-    #     ])
-    # model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
-    # model.fit(train,label,batch_size=3,epochs=8)
-    # test, t_label = loadtest()
-    # test_loss, test_acc = model.evaluate(test,t_label)
-    # print("The test accuracy", test_acc)
-    # model.save("hvm.h5")
+    test = keras.preprocessing.image_dataset_from_directory(
+        "validation",
+        seed=123,
+        image_size=(300,300),
+        batch_size=345
+    )
+    class_names = test.class_names
+    print(class_names)
+    # THis is without normalization
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(300,300,3)),
+        keras.layers.Dense(130,activation="relu"),
+        keras.layers.Dense(80,activation="relu"),
+        keras.layers.Dense(2,activation="softmax")
+        ])
+    model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+    history = model.fit(train,validation_data=test,epochs=10)
+    test, t_label = loadtest()
+    test_loss, test_acc = model.evaluate(test,t_label)
+    print("The test accuracy", test_acc)
+    model.save("hvm.h5")
     # prediction = model.predict(test)
     # for i in range(20):
     #     plt.grid(False)
